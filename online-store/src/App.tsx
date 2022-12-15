1673c759f02aeffa4084a29cf395f08e7fc3dca5
Danil <danylo.makhov@nure.ua>
2022-12-15 16:47:39 +0300

    implement main page and store

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import './styles/css/Template.css';
-
+import { useEffect, useState } from 'react';
+import Products from './components/Main/Products/Products';
+import { getAllProducts } from './controllers/getAllProducts';
+import { setProducts } from './controllers/setProducts';
+import Busket from './pages/Busket/Busket';
 function App() {
+  const [loading, setLoading] = useState(true)
+  const setProduct = () => {
+    fetch('https://dummyjson.com/products?limit=100').then((data) => {
+      data.json().then((product) => {
+        setProducts(product.products);
+      });
+    }).then(() => {
+      getAllProducts().length === 0 ? setProduct() : setLoading(false)
+    }).catch((err) => {
+      if(err) {
+        return err
+      }
+    });
+  }
+  useEffect(() => {
+    setProduct()
+  }, []);
   return (
     <div className="App">
-      
+      {loading ? <div>Loading please wait...</div> : (<Products />)}
     </div>
-  );
+  )
 }
 
 export default App;
