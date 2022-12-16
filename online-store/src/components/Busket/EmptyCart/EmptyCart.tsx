533b67eae6e4cf881409694c710c946e2d097f47
Ayrat <axmetxan@gmail.com>
2022-12-16 12:07:29 +0300

    fix: cart icon source

--- online-store/src/components/Busket/EmptyCart/EmptyCart.tsx
+++ online-store/src/components/Busket/EmptyCart/EmptyCart.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './EmptyCart.scss';
+import cartIcon from './../../../assets/images/shopping-cart.png';
 
 function EmptyCart() {
   return (
     <div className='empty-cart'>
-      <img className='cart-icon' alt="cart-icon" />
+      <img src={cartIcon} className='cart-icon' alt="cart-icon" />
       <span className='empty-cart-title'>Cart is empty</span>
     </div>
   )
