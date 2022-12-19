289e3153ff3f529fd9342f43c2ae3ee579d084f8
Ayrat <axmetxan@gmail.com>
2022-12-19 19:08:55 +0300

    fix: change store intarface

--- online-store/src/types/Response.ts
+++ online-store/src/types/Response.ts
@@ -1,26 +1,35 @@
 export interface Response{
-    brand: string,
-    category: string,
-    description: string,
-    discountPercentage: number,
-    id: number,
-    images: string[],
-    price: number,
-    rating: number,
-    stock: number,
-    thumbnail: string,
-    title: string
+  brand: string,
+  category: string,
+  description: string,
+  discountPercentage: number,
+  id: number,
+  images: string[],
+  price: number,
+  rating: number,
+  stock: number,
+  thumbnail: string,
+  title: string
 }
 export interface Store {
-    state: {
-        products: Response[],
-        busket: Response[],
-        filters: {}
-    },
-    setProducts(array: Response[]): void,
-    getAllProducts(): Response[],
-    addToBusket(product: Response): void,
-    getAllBusketItems(): Response[],
-    busketIsEmpty(): boolean,
-    removeFromBusket(id: Response): void
+  state: {
+      products: Response[],
+      busket: {
+        items: {
+          [key: number]: {
+            items: Response[],
+            totalPrice: number,
+          },
+        },
+        totalCount: number,
+        totalPrice: number,
+      },
+      filters: {}
+  },
+  setProducts(array: Response[]): void,
+  getAllProducts(): Response[],
+  addToBusket(product: Response): void,
+  getAllBusketItems(): Response[],
+  busketIsEmpty(): boolean,
+  removeFromBusket(id: Response): void
 }
