bb87b4aa96f1e6bd10b7bd2a38a0237d42a98f05
Ayrat <axmetxan@gmail.com>
2022-12-29 20:56:23 +0300

    fix: local storage busket state

---
+++ online-store/src/store/constants.ts
@@ -0,0 +1,15 @@
+export const busketDefaultState = JSON.stringify({
+  cartProductsData: {},
+  promo: {
+    percentageDiscounts: {
+      'NEWYEAR10': 10,
+      'LUCKY10':  10,
+    },
+    userPromoCodes: [],
+  },
+  cartTotalCount: 0,
+  cartTotalPrice: 0,
+  cartTotalCards: 0,
+  totalDiscounAmount: 0,
+  cartGrandTotal: 0
+});
