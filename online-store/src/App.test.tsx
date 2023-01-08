2d85b5f106f71e9b4999c7913cd4a42799dd2ad4
Ayrat <axmetxan@gmail.com>
2023-01-08 16:21:32 +0300

    feat: implement EmptyCart.test

--- online-store/src/App.test.tsx
+++ online-store/src/App.test.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { store } from './store/store';
-import { BusketState } from './types/Response'
 
 test('renders learn react link', () => {
   render(<App />);
